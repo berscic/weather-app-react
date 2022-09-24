0a4bdf667fcd9dbceeafb3c5f492efb076b175df
berscic <109431600+berscic@users.noreply.github.com>
2022-09-24 21:17:41 +0200

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ export default function App() {
     <div className="container">
       <p className="App">
         This project has been coded by Bernice Scicluna and is{" "}
-        <a href="/">Open Sourced</a> on GitHub
+        <a href="https://github.com/berscic/weather-app-react">Open Sourced</a>{" "}
+        on GitHub
       </p>
     </div>
   );
